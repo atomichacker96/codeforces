68ca7b2212e0ba8f94737de33e0ceda6dd3ba4c2
zafarshodmonov1996 <zafarshodmonov1996@gmail.com>
2025-09-14 06:55:09 +0500

    atcoder-422-A

---
+++ atcoder_422_B.py
@@ -0,0 +1,7 @@
+# atcoder_422_B.py
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
--- new.py
+++ new.py
@@ -3,14 +3,16 @@ import os
 
 # Argumentlarni pars qilish
 parser = argparse.ArgumentParser(description="Yangi Codeforces problem fayl yaratish")
+parser.add_argument("platform", type=str, help="Platform nomi, masalan: codeforces")
 parser.add_argument("contest_number", type=str, help="Contest raqami, masalan: 2020")
 parser.add_argument("problem_letter", type=str, help="Problem harfi, masalan: A")
 args = parser.parse_args()
 
 # Fayl nomini yaratish
+plt = args.platform.lower()
 contest = args.contest_number
 problem = args.problem_letter.upper()  # katta harfga o'tkazamiz
-file_name = f"problem_{contest}_{problem}.py"
+file_name = f"{plt}_{contest}_{problem}.py"
 
 # Fayl yaratish va shablon yozish (agar mavjud bo'lmasa)
 if not os.path.exists(file_name):
